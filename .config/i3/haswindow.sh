858028ef0bdc4b62d362162124ec6ce70ce0c63b
Wenir <wenirr@gmail.com>
2016-09-11 23:50:04 +0300

    Migrate to i3 WM

---
+++ .config/i3/haswindow.sh
@@ -0,0 +1,8 @@
+xprop -root                              |
+grep '_NET_CLIENT_LIST_STACKING(WINDOW)' |
+grep -ohw 0x.......                      |
+while read line;
+do
+    xprop -id "$line";
+done                                     |
+grep $1
